e66667dc8e78ffadc0457786ad2f8fb8e62328bb
gIliev-120 <g.iliev96@gmail.com>
2014-07-04 00:34:54 +0300

    Making some changes 2of3 Buttons working but there is a little bug

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
 document.addEventListener("DOMContentLoaded", init, false);
-function init(){
+
+function init()
+{
   var canvas = document.getElementById("canvas");
-  canvas.addEventListener("mousedown", getPosition, false);
+  var rectButton = document.getElementById('rectButton');
+  var circleButton = document.getElementById('circleButton');
+
+  rectButton.onclick = function(){
+    canvas.addEventListener("click",drawRect,false);
+  }
+  circleButton.onclick=function(){
+   canvas.addEventListener("click",drawCircle,false); 
+ }
 }
 
 function getPosition(event)
 {
+ var x = new Number();
+ var y = new Number();
+ var canvas = document.getElementById("canvas");
+ var context = canvas.getContext('2d');
 
-  var x = new Number();
-  var y = new Number();
+ x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+ y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+ return [x,y];
+}
+
+function drawRect(event){
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext('2d');
-
-  x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-  y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-  
-
-  x -= canvas.offsetLeft;
-  y -= canvas.offsetTop;
-
-  function drawRect(){
-    context.beginPath();
-    context.strokeRect(x,y,30,30);			
-    context.stroke();
-  }
-
-  function drawCircle() {
-    context.beginPath();
-    context.arc(x,y,5,1,80);
-    context.stroke();
-  }
-
-  //drawRect();
-  drawCircle();
-
-
+  x=getPosition(event)[0] - canvas.offsetLeft;
+  y=getPosition(event)[1] - canvas.offsetTop;
+  context.beginPath();
+  context.strokeRect(x,y,30,30);  
+  context.stroke();
 }
+
+function drawCircle(event) {
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d');
+  x=getPosition(event)[0] - canvas.offsetLeft;
+  y=getPosition(event)[1] - canvas.offsetTop;
+  context.beginPath();
+  context.arc(x,y,5,1,80);
+  context.stroke();
+}
+
+
