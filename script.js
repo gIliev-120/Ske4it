8603339b90041cfedc2a14c321de7c72c36950b1
gIliev-120 <g.iliev96@gmail.com>
2014-07-09 22:03:57 +0300

    Little changes

--- script.js
+++ script.js
@@ -1,68 +1,63 @@
-document.addEventListener("DOMContentLoaded", init, false);
 
+document.addEventListener("DOMContentLoaded", init, false);
+var canvas;
+var context;
 var lastClick=[0,0];
 var numOfClicks=0;
 
-
-//var colors = ["#e2c9b9","#4b9484","#434f7b","#b5e622","#ffbf00"];
-//var colorsLength=colors.length;
 function init()
 {
-  var check=0
-  var canvas = document.getElementById("canvas");
- canvas.width = (window.innerWidth-13);
- canvas.height = (window.innerHeight-27);
+  canvas = document.getElementById("canvas");
+  canvas.width = (window.innerWidth-13);
+  canvas.height = (window.innerHeight-27);
+  context = canvas.getContext('2d');
   var rectButton = document.getElementById('rectButton');
   var circleButton = document.getElementById('circleButton');
   var lineButton = document.getElementById('lineButton');
   var colorButton = document.getElementById('colorButton');
+  var clearButton = document.getElementById('clearButton');
+
   var imported = document.createElement('script');
   imported.src = 'jscolor\\jscolor.js';
   document.body.appendChild(imported);
 
-
   colorButton.onclick = function(){
-    
-    switchColor();
-    
+    switchColor(context);
+  }
+  clearButton.onclick=function(){
+    canvas.width = canvas.width;
   }
 
   rectButton.onclick = function(){
-    canvas.removeEventListener("click",drawCircle,false);
-    canvas.removeEventListener("click",drawLine,false);
-
-    canvas.addEventListener("click",drawRect,false);
+  canvas.removeEventListener("click",drawCircle);
+  canvas.removeEventListener("click",drawLine);
+   canvas.addEventListener("click",drawRect);
   }
+
   circleButton.onclick=function(){
-   canvas.removeEventListener("click",drawLine,false);
-   canvas.removeEventListener("click",drawRect,false);
-   canvas.addEventListener("click",drawCircle,false); 
- }
+   canvas.removeEventListener("click",drawLine);
+   canvas.removeEventListener("click",drawRect);
+   canvas.addEventListener("click",drawCircle); 
+  }
+
  lineButton.onclick=function(){
-  canvas.removeEventListener("click",drawRect,false);
-  canvas.removeEventListener("click",drawCircle,false);
-   canvas.addEventListener("click",drawLine,false);
+  canvas.removeEventListener("click",drawRect);
+  canvas.removeEventListener("click",drawCircle);
+  canvas.addEventListener("click",drawLine);
  }
 
-}
+function getPosition(event){
+  var x = new Number();
+  var y = new Number();
 
-function getPosition(event)
-{
- var x = new Number();
- var y = new Number();
- var canvas = document.getElementById("canvas");
- var context = canvas.getContext('2d');
-
- x = event.clientX; 
- y = event.clientY; 
+  x = event.clientX; 
+  y = event.clientY; 
  
-
- return [x,y];
+  return [x,y];
 }
 
 function drawRect(event){
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext('2d');
+
   x=getPosition(event)[0] - canvas.offsetLeft;
   y=getPosition(event)[1] - canvas.offsetTop;
 
@@ -72,10 +67,10 @@ function drawRect(event){
 }
 
 function drawCircle(event) {
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext('2d');
+
   x=getPosition(event)[0] - canvas.offsetLeft;
   y=getPosition(event)[1] - canvas.offsetTop;
+
   context.beginPath();
   context.arc(x,y,5,1,80);
   context.stroke();
@@ -83,47 +78,39 @@ function drawCircle(event) {
 
 
 function drawLine(event){
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext('2d');
-  x=getPosition(event)[0] - canvas.offsetLeft;
+
+ x=getPosition(event)[0] - canvas.offsetLeft;
   y=getPosition(event)[1] - canvas.offsetTop;
   
   
     //console.log(x);
     //console.log(y);
 
-      if (numOfClicks != 1) {
-        numOfClicks++;
+    if (numOfClicks != 1) {
+      numOfClicks++;
     } else {
-        context.beginPath();
-        context.moveTo(lastClick[0], lastClick[1]);
-        context.lineTo(x, y, 6);
-        
-        context.stroke();
-        
-        numOfClicks = 0;
+      context.beginPath();
+      context.moveTo(lastClick[0], lastClick[1]);
+      context.lineTo(x, y, 6);
+
+      context.stroke();
+
+      numOfClicks = 0;
     }
     
     lastClick = [x, y];
 
     //console.log(lastClick[0]);
     //console.log(lastClick[1]);
- }
+  }
 
- function switchColor(){
-        colorButton.color.showPicker();    
-        var canvas = document.getElementById("canvas");
-        var context = canvas.getContext('2d');
-        var myPicker = new jscolor.color(document.getElementById('colorButton'));
-        var choosedColor=myPicker.toString();
-        var convertedColor="#"+choosedColor+"";
-        //console.log(a)
-        
-        
-                
-        context.strokeStyle=convertedColor;
+  function switchColor(context){
+    var myPicker = new jscolor.color(document.getElementById('colorButton'));
+    var choosedColor=myPicker.toString();
+    var convertedColor="#"+choosedColor+"";
 
-      //  colorButton.color.hidePicker();
+    context.strokeStyle=convertedColor;
     }
 
+}
 
